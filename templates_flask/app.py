e529f6dc668e0fe099a50450144876b4d69b1387
Anas-kenawi <kenawi3@gmail.com>
2023-05-05 09:47:40 +0000

    versione 4ù

---
+++ templates_flask/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import render_template
+import mysql.connector
+import pandas as pd
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password=""
+)
+mycursor = mydb.cursor()
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def player_list():
+      mycursor.execute("SELECT * FROM INAZUMA_ELEVEN.players")
+      myresult = mycursor.fetchall()
+      return render_template('site.html', players=myresult)
