6a4b2da1256f44379c058cf2cc2633c57dbdc2f9
Anas-kenawi <kenawi3@gmail.com>
2023-05-19 11:55:55 +0000

    versione_8

--- templates_flask/app.py
+++ templates_flask/app.py
@@ -18,3 +18,10 @@ def player_list():
       mycursor.execute("SELECT * FROM INAZUMA_ELEVEN.players")
       myresult = mycursor.fetchall()
       return render_template('site.html', players=myresult)
+
+@app.route("/Mark_Evans")
+def Mark_Evans():
+      mycursor.execute("SELECT * FROM INAZUMA_ELEVEN.players")
+      myresult = mycursor.fetchall()
+      return render_template('Mark.html', players=myresult)
+
